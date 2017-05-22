35492e0b5a70a85af388a7eefc57d2e168ecd284
Martín Isusi Seff <martinisusi@hotmail.com>
2017-05-22 16:39:06 -0300

    Updated test program.

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,10 +1,11 @@
 package test;
 
 import sorter.*;
+import java.util.Date;
 
 public class Test {
 	public static void main(String[] args) {
-		final int length= 1000;
+		final int length= 10;
 		
 		int[] array1= new int[length];
 		int[] array2= new int[length];
@@ -15,13 +16,21 @@ public class Test {
 			array2[i]=num;
 		}
 		System.out.print("Bubble sort started... ");
+		Date start = new Date();
 		long operationsPerformedBubbleSort= Sorter.sort(array1, SortOrder.ASCENDANT, SortingMethod.BUBBLE_SORT);
-		System.out.print("Bubble sort ended. ");
-		System.out.println(operationsPerformedBubbleSort + " operations performed to order the array using bubble sort");
+		double timeTaken= ((new Date()).getTime() - start.getTime())/(double)1000;
+		System.out.println("Bubble sort ended. ");
+		System.out.println("\t" + operationsPerformedBubbleSort + " operations performed aprox.");
+		System.out.println("\tTook " + timeTaken + " seconds aprox. to complete.");
+		
+		System.out.println();
 		
 		System.out.print("Merge sort started... ");
+		start = new Date();
 		long operationsPerformedMergeSort= Sorter.sort(array2, SortOrder.ASCENDANT, SortingMethod.MERGE_SORT);
-		System.out.print("Merge sort ended. ");
-		System.out.println(operationsPerformedMergeSort + " operations performed to order the array using merge sort");
+		timeTaken= ((new Date()).getTime() - start.getTime())/(double)1000;
+		System.out.println("Merge sort ended. ");
+		System.out.println("\t" + operationsPerformedMergeSort + " operations performed aprox.");
+		System.out.println("\tTook " + timeTaken + " seconds aprox. to complete.");
 	}
 }
